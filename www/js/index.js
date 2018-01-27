b8cf3ca4a12a5c9184c5f187485c6cde1c2fd2b5
takumadx <hirayama.takuma@ohmydigifab.com>
2018-01-27 17:39:43 +0900

    buys

--- www/js/index.js
+++ www/js/index.js
@@ -14,6 +14,14 @@ function get_buys() {
 	loadFile(server + "/api/get_buys", function(text) {
 		var buys = JSON.parse(text);
 		console.log(buys);
+		for (var i = 0; i < buys.length; i++) {
+			var buy = buys[i];
+			var template = document.querySelector('#buy_template').innerHTML;
+			for (key in buy) {
+				template = template.replace("%" + key + "%", buy[key]);
+			}
+			$("#buys").append(template);
+		}
 	});
 }
 
